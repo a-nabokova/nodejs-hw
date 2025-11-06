17b587eb3fe9e2696d3a618b3fb67206c410b391
a-nabokova <itcw2s@gmail.com>
2025-11-06 22:12:25 +0200

    hw update

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -3,10 +3,10 @@ import { getAllNotes , getNoteById } from '../controllers/notesController.js';
 
 const router = Router();
 
-export default router;
 
 router.get('/notes',  getAllNotes );
 
 
 router.get('/notes/:noteId', getNoteById);
 
+export default router;
